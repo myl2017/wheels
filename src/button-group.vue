0a3e875302a3b8c234312fc7ca61da6cb67d27b9
706695013@qq.com <706695013@qq.com>
2021-07-12 12:08:36 +0800

    检测子元素是不是 button

---
+++ src/button-group.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="g-button-group">
+		<slot/>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'button-group',
+    mounted() {
+      for (let node of this.$el.children) {
+        let name = node.nodeName.toLocaleLowerCase()
+        if (name !== 'button') {
+          console.warn(`g-button-group 的子元素应该全是 g-button， 但是你写的是 ${name}`)
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+	.g-button-group {
+		display: inline-flex;
+		vertical-align: middle;
+
+		> .g-button {
+			border-radius: 0;
+
+			/*&:not(:first-child) {*/
+			/*	border-left: none;*/
+			/*}*/
+			margin-left: -1px;
+
+			&.first-child {
+				border-top-left-radius: var(--border-radius);
+				border-bottom-left-radius: var(--border-radius);
+			}
+
+			&.last-child {
+				border-top-left-radius: var(--border-radius);
+				border-bottom-right-radius: var(--border-radius);
+			}
+
+			&:hover {
+				position: relative;
+				z-index: 1;
+			}
+		}
+	}
+</style>
