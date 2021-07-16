7775ebe7b6a7b1c0d7e7a7b7a2eebb0ab2328194
706695013@qq.com <706695013@qq.com>
2021-07-16 11:16:03 +0800

    让 input 支持 v-model 双向绑定

--- src/input.vue
+++ src/input.vue
@@ -1,12 +1,14 @@
 <template>
 	<div class="wrapper" :class="{error}">
 		<input v-bind:value="value" type="text" :disabled="disabled" :readonly="readonly"
-					 @change="$emit('change', $event)"
-					 @input="$emit('input', $event)"
-					 @focus="$emit('focus', $event)"
-					 @blur="$emit('blur', $event)">
+					 @change="$emit('change', $event.target.value)"
+					 @input="$emit('input', $event.target.value)"
+					 @focus="$emit('focus', $event.target.value)"
+					 @blur="$emit('blur', $event.target.value)">
 		<template v-if="error">
-			<icon v-if="error" name="error" class="icon-error"></icon>
+			<icon v-if="error" name="error" class="icon-error">
+
+			</icon>
 			<span class="errorMessage">姓名不能少于两个字</span>
 		</template>
 	</div>
