e7f12402e4d4707ff044a7a58b8285836ab2c1ea
706695013@qq.com <706695013@qq.com>
2021-08-04 17:47:15 +0800

    实现了只能有一个 toast 的功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast" ref="wrapper">
+  <div class="toast" ref="wrapper" :class="toastClasses">
     <div class="message">
       <slot v-if="!enableHtml"></slot>
       <div v-else v-html="$slots.default[0]"></div>
@@ -35,6 +35,18 @@ export default {
     enableHtml: {
       type: Boolean,
       default: false
+    },
+    position: {
+      type: String,
+      default: 'top',
+      validator(value) {
+        return ['top', 'bottom', 'middle'].indexOf(value) >= 0
+      }
+    }
+  },
+  computed: {
+    toastClasses() {
+      return {[`position-${this.position}`]: true}
     }
   },
   created() {
@@ -104,9 +116,25 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   }
 
   .line {
-    height: 100%;
     border-left: 1px solid #666;
     margin-left: 16px;
   }
+
+  &.position-top {
+    top: 0;
+    transform: translateX(-50%);
+  }
+
+  &.position-bottom {
+    bottom: 0;
+    transform: translateX(-50%);
+  }
+
+  &.position-middle {
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+
 }
 </style>
