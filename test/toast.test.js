ab4c8f66836b35acfe02b45111dd4107d80a69d9
706695013@qq.com <706695013@qq.com>
2021-08-05 17:13:04 +0800

    toast 测试用例 bug 处理

--- test/toast.test.js
+++ test/toast.test.js
@@ -28,7 +28,7 @@ describe('Toast', () => {
             })
         })
 
-        it('接受 closeButton', () => {
+        it('接受 closeButton', (done) => {
             const callback = sinon.fake()
             const Constructor = Vue.extend(Toast)
             const vm = new Constructor({
@@ -41,8 +41,11 @@ describe('Toast', () => {
             }).$mount()
             let closeButton = vm.$el.querySelector('.close')
             expect(closeButton.textContent.trim()).to.eq('关闭吧')
-            closeButton.click()
-            expect(callback).to.have.been.called
+            setTimeout(()=> {
+                closeButton.click()
+                expect(callback).to.have.been.called
+                done()
+            },1000)
 
         })
 
