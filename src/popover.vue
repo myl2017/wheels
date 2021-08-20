c1876794a62dfbfbe3c51820094de02b677b1f77
706695013@qq.com <706695013@qq.com>
2021-08-20 09:57:35 +0800

    实现最简单的 popover

---
+++ src/popover.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="popover" @click.stop="xxx">
+    <div class="content-wrapper" v-if="visible" @click.stop>
+      <slot name="content"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GuluPopover",
+  data() {
+    return {visible: false}
+  },
+  methods: {
+    xxx() {
+      this.visible = !this.visible
+      console.log('切换 visible')
+      if (this.visible === true) {
+        this.$nextTick(() => {
+          let eventHandle = () => {
+            console.log('点击body就关闭popover')
+            this.visible = false
+            console.log('删除监听器')
+            document.removeEventListener('click', eventHandle)
+          }
+          document.addEventListener('click', eventHandle)
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+
+  .content-wrapper {
+    position: absolute;
+    bottom: 100%;
+    left: 0;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  }
+}
+</style>
