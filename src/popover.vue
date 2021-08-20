76e6298ef898caf82b9d78f32fddf370cd5fdc25
706695013@qq.com <706695013@qq.com>
2021-08-20 15:24:57 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block;">
      <slot></slot>
     </span>
   </div>
@@ -25,10 +25,17 @@ export default {
       this.$refs.contentWrapper.style.left = left + window.screenX + 'px'
       this.$refs.contentWrapper.style.top = top + window.screenY + 'px'
     },
-    onClickDocument (e) {
+    onClickDocument(e) {
       if (this.$refs.popover &&
           (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
-      ) { return; }
+      ) {
+        return;
+      }
+      if (this.$refs.contentWrapper &&
+          (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
+      ) {
+        return;
+      }
       this.close()
     },
     open() {
@@ -56,6 +63,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
@@ -64,8 +73,37 @@ export default {
 
 .content-wrapper {
   position: absolute;
-  border: 1px solid red;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  //box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+  background: white;
   transform: translateY(-100%);
+  margin-top: -10px;
+  padding: 0.5em 1em;
+  max-width: 20em;
+  word-break: break-all;
+
+  &::before, &::after {
+    content: '';
+    display: block;
+    border: 10px solid transparent;
+    border-top-color: black;
+    width: 0;
+    height: 0;
+    position: absolute;
+    top: 100%;
+    left: 10px;
+  }
+
+  &::after {
+    border-top-color: black;
+    top: 100%;
+  }
+
+  &::after {
+    border-top-color: white;
+    top: calc(100% - 1px);
+  }
 }
 </style>
