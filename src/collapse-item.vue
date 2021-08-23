d13072937ce347220c7af13a49cabaf090b60eff
706695013@qq.com <706695013@qq.com>
2021-08-23 16:52:49 +0800

    支持可以设置默认 selected项和单个选中

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -24,16 +24,17 @@ export default {
   },
   data() {
     return {
-      open: false
+      open: false,
+      single: false
     }
   },
   inject: ['eventBus'],
   mounted() {
-    this.eventBus && this.eventBus.$on('update:selected', (name) => {
-      if (name !== this.name) {
-        this.close()
+    this.eventBus && this.eventBus.$on('update:selected', (names) => {
+      if (names.indexOf(this.name) >= 0) {
+        this.open = true
       } else {
-        this.show()
+        this.open = false
       }
     })
 
@@ -41,17 +42,10 @@ export default {
   methods: {
     toggle() {
       if (this.open) {
-        this.open = false
+        this.eventBus && this.eventBus.$emit('update:removeSelected', this.name)
       } else {
-        this.open = true
-        this.eventBus && this.eventBus.$emit('update:selected', this.name)
+        this.eventBus && this.eventBus.$emit('update:addSelected', this.name)
       }
-    },
-    close() {
-      this.open = false
-    },
-    show() {
-      this.open = true
     }
   }
 }
