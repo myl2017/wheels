4ebe982b2bb7da853672d21f3189d5f9560bedad
706695013@qq.com <706695013@qq.com>
2021-08-25 11:13:21 +0800

    创建 cascader 组件

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,17 +1,35 @@
 <template>
-<div class="cascader">
-  <slot></slot>
-</div>
+  <div class="cascader">
+    <div class="trigger">
+      <slot></slot>
+    </div>
+    <div class="popover">
+      <div v-for="item in dataSource">
+        <cascader-item :source-item="item">{{ item.name }}</cascader-item>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import CascaderItem from './cascader-item'
+
 export default {
-  name: "GuluCascader"
+  name: "GuluCascader",
+  components: {
+    CascaderItem
+  },
+  props: {
+    dataSource: {
+      type: Array
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 @import "var";
+
 .cascader {
 
 }
