0b9b340c2f4e217f2596b0fd4f046a352a4f1fe8
706695013@qq.com <706695013@qq.com>
2021-08-25 09:52:18 +0800

    将 scss 变量提升到 var.scss

--- src/input.vue
+++ src/input.vue
@@ -1,106 +1,93 @@
 <template>
-	<div class="wrapper" :class="{error}">
-		<input v-bind:value="value" type="text" :disabled="disabled" :readonly="readonly"
-					 @change="$emit('change', $event.target.value)"
-					 @input="$emit('input', $event.target.value)"
-					 @focus="$emit('focus', $event.target.value)"
-					 @blur="$emit('blur', $event.target.value)">
-		<template v-if="error">
-			<icon v-if="error" name="error" class="icon-error">
+  <div class="wrapper" :class="{error}">
+    <input v-bind:value="value" type="text" :disabled="disabled" :readonly="readonly"
+           @change="$emit('change', $event.target.value)"
+           @input="$emit('input', $event.target.value)"
+           @focus="$emit('focus', $event.target.value)"
+           @blur="$emit('blur', $event.target.value)">
+    <template v-if="error">
+      <icon v-if="error" name="error" class="icon-error">
 
-			</icon>
-			<span class="errorMessage">姓名不能少于两个字</span>
-		</template>
-	</div>
+      </icon>
+      <span class="errorMessage">姓名不能少于两个字</span>
+    </template>
+  </div>
 </template>
 
 <script>
-  import Icon from './icon'
+import Icon from './icon'
 
-  export default {
-    name: 'GuluInput',
-    components: {Icon},
-    props: {
-      value: {
-        type: String
-      },
-      disabled: {
-        type: Boolean,
-        default: false
-      },
-      readonly: {
-        type: Boolean,
-        default: false
-      },
-      error: {
-        type: String
-      }
+export default {
+  name: 'GuluInput',
+  components: {Icon},
+  props: {
+    value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-	/*--button-height: 32px;*/
-	/*--font-size: 14px;*/
-	/*--button-bg: white;*/
-	/*--button-active-bg: #eee;*/
-	/*--border-radius: 4px;*/
-	/*--color: #333;*/
-	/*--border-color: #999;*/
-	/*--border-color-hover: #666;*/
-	$height: 32px;
-	$border-color: #999;
-	$border-color-hover: #666;
-	$border-radius: 4px;
-	$font-size: 12px;
-	$box-shadow-color: rgba(0, 0, 0, 0.5);
-	$red: #F1453D;
-	.wrapper {
-		font-size: $font-size;
-		display: inline-flex;
-		align-items: center;
+@import "var";
+$height: $input-height;
+.wrapper {
+  font-size: $font-size;
+  display: inline-flex;
+  align-items: center;
 
-		> :not(:last-child) {
-			margin-right: .5em;
-		}
+  > :not(:last-child) {
+    margin-right: .5em;
+  }
 
-		> input {
-			height: 32px;
-			border: 1px solid $border-color;
-			border-radius: $border-radius;
-			padding: 0 8px;
-			font-size: inherit;
+  > input {
+    height: 32px;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    padding: 0 8px;
+    font-size: inherit;
 
-			&:hover {
-				border-color: $border-color-hover;
-			}
+    &:hover {
+      border-color: $border-color-hover;
+    }
 
-			&:focus {
-				box-shadow: inset 0 1px 3px $box-shadow-color;
-				outline: none;
-			}
+    &:focus {
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
 
-			&[disabled], &[readonly] {
-				border-color: #bbb;
-				color: #bbb;
-				cursor: not-allowed;
-			}
-		}
+    &[disabled], &[readonly] {
+      border-color: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
+    }
+  }
 
-		&.error {
-			> input {
-				border-color: $red;
-			}
-		}
+  &.error {
+    > input {
+      border-color: $red;
+    }
+  }
 
-		.icon-error {
-			width: 1.5em;
-			height: 1.5em;
-			fill: $red;
-		}
+  .icon-error {
+    width: 1.5em;
+    height: 1.5em;
+    fill: $red;
+  }
 
-		.errorMessage {
-			color: $red;
-		}
-	}
+  .errorMessage {
+    color: $red;
+  }
+}
 </style>
