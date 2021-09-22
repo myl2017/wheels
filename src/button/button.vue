9c4c89df43b88a586d178de90f1c1fc7ecb3ac57
706695013@qq.com <706695013@qq.com>
2021-09-22 10:14:10 +0800

    移动 button.vue 和 button-group.vue

--- src/button/button.vue
+++ src/button/button.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import Icon from './icon'
+import Icon from '../icon'
 
 export default {
   name: 'GuluButton',
@@ -36,7 +36,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "var";
+@import "src/var";
 
 @keyframes spin {
   0% {
