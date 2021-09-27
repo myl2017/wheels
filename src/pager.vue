5dfba19e297703daf30fc230796d17fd2a15d38f
706695013@qq.com <706695013@qq.com>
2021-09-27 15:15:35 +0800

    完成大部份样式

--- src/pager.vue
+++ src/pager.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="gulu-pager">
-    <span v-for="page in pages" class="gulu-pager-item" :class="{active: page === currentPage}">{{ page }}</span>
+    <span class="gulu-pager-nav prev" :class="{disabled: currentPage === 1}">
+      <g-icon name="left"></g-icon>
+    </span>
+    <template v-for="page in pages">
+      <template v-if="page === currentPage">
+        <span class="gulu-pager-item current">{{ page }}</span>
+      </template>
+      <template v-else-if="page === '...'">
+        <g-icon name="dots" class="gulu-pager-separator">?</g-icon>
+      </template>
+      <template v-else>
+        <span class="gulu-pager-item other">{{ page }}</span>
+      </template>
+    </template>
+    <span class="gulu-pager-nav next" :class="{disabled: currentPage === totalPage}">
+      <g-icon name="right"></g-icon>
+    </span>
   </div>
 </template>
 <style scoped lang="scss">
 @import "styles/var";
 
 .gulu-pager {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  $width: 20px;
+  $height: 20px;
+  $font-size: 12px;
+
+  &-separator {
+    width: $width;
+    font-size: $font-size;
+  }
+
   &-item {
     border: 1px solid #e1e1e1;
     border-radius: $border-radius;
@@ -14,24 +42,45 @@
     display: inline-flex;
     justify-content: center;
     align-items: center;
-    font-size: 12px;
-    min-width: 20px;
-    height: 20px;
+    font-size: $font-size;
+    min-width: $width;
+    height: $height;
     margin: 0 4px;
     cursor: pointer;
-    &.active, &:hover {
+
+    &.current, &:hover {
       border-color: $blue;
     }
-    &.active {
+
+    &.current {
       cursor: default;
+    }
+  }
+  &-nav {
+    margin: 0 4px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: $width;
+    height: $height;
+    background: $grey;
+    border-radius: $border-radius;
+    font-size: $font-size;
+    &.disabled {
+      svg {
+        fill: darken($grey, 30%);
+      }
     }
   }
 }
 </style>
 
 <script>
+import GIcon from './icon'
+
 export default {
   name: "GuluPager",
+  components: {GIcon},
   props: {
     totalPage: {
       type: Number,
@@ -51,6 +100,7 @@ export default {
       this.currentPage,
       this.currentPage - 1, this.currentPage - 2,
       this.currentPage + 1, this.currentPage + 2]
+        .filter((n)=> n >= 1 && n <= this.totalPage)
         .sort((a, b) => a - b))
         .reduce((prev, current, index, array) => {
           prev.push(current)
