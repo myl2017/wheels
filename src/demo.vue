42b67b330f59be1082afc27d10545a99eed04b55
706695013@qq.com <706695013@qq.com>
2021-09-27 11:27:03 +0800

    实现 pager 基本结构和样式

--- src/demo.vue
+++ src/demo.vue
@@ -1,92 +1,20 @@
 <template>
-  <div>
-    <g-nav :selected.sync="selected" style="margin: 20px;" @update:selected="onChange">
-      <g-nav-item name="home">
-        <a href="https://jirengu.com" target="_blank">首页</a>
-      </g-nav-item>
-      <g-sub-nav name="about">
-        <template slot="title">关于</template>
-        <g-nav-item name="culture">企业文化</g-nav-item>
-        <g-nav-item name="developers">开发团队</g-nav-item>
-        <g-sub-nav name="contacts">
-          <template slot="title">联系方式</template>
-          <g-nav-item name="wechat">微信</g-nav-item>
-          <g-nav-item name="qq">QQ</g-nav-item>
-          <g-sub-nav name="phone">
-            <template slot="title">手机</template>
-            <g-nav-item name="cm">移动</g-nav-item>
-            <g-nav-item name="cu">联通</g-nav-item>
-            <g-nav-item name="cn">电信</g-nav-item>
-          </g-sub-nav>
-        </g-sub-nav>
-
-      </g-sub-nav>
-      <g-nav-item name="hire">招聘</g-nav-item>
-    </g-nav>
-
-    <g-nav :selected.sync="selected" vertical style="margin: 20px; margin-top: 200px">
-      <g-nav-item name="home">首页</g-nav-item>
-      <g-sub-nav name="about">
-        <template slot="title">关于</template>
-        <g-nav-item name="culture">企业文化</g-nav-item>
-        <g-nav-item name="developers">开发团队</g-nav-item>
-        <g-sub-nav name="contacts">
-          <template slot="title">联系方式</template>
-          <g-nav-item name="wechat">微信</g-nav-item>
-          <g-nav-item name="qq">QQ</g-nav-item>
-          <g-sub-nav name="phone">
-            <template slot="title">手机</template>
-            <g-nav-item name="cm">移动</g-nav-item>
-            <g-nav-item name="cu">联通</g-nav-item>
-            <g-nav-item name="cn">电信</g-nav-item>
-          </g-sub-nav>
-        </g-sub-nav>
-
-      </g-sub-nav>
-      <g-nav-item name="hire">招聘</g-nav-item>
-    </g-nav>
-
-    <p>你好，我是张三！</p>
+  <div style="margin: 20px;">
+    <g-pager :total-page="20" :current-page="3"></g-pager>
   </div>
 </template>
 
 <script>
-import GNav from './nav/nav.vue'
-import GNavItem from './nav/nav-item.vue'
-import GSubNav from './nav/sub-nav.vue'
+import GPager from './pager'
 
 export default {
   name: "demo",
-  components: {GNav, GNavItem, GSubNav},
-  data() {
-    return {
-      selected: 'culture'
-    };
-  },
-  created() {
-
-  },
-  methods: {
-    onChange(selected) {
-      if (selected === 'home') {
-        alert('hi')
-      }
-    }
-  },
-  watch: {
-    selected(newValue) {
-      if (newValue === 'home') {
-
-      }
-    }
-  }
+  components: {GPager},
 };
 </script>
-
+<style>
+* {margin: 0;padding: 0;box-sizing: border-box;}
+body { background: white;}
+</style>
 <style scoped lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 </style>
