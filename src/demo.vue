681e493e67af8101625d0350996649c4d6f7c4c7
706695013@qq.com <706695013@qq.com>
2021-10-19 15:14:40 +0800

    uploader 组件完成

--- src/demo.vue
+++ src/demo.vue
@@ -1,79 +1,31 @@
 <template>
-  <div>
-    <div style="margin: 20px;">
-      <g-table :columns="columns" :data-soruce="dataSource" bordered :selected-items.sync="selected"
-               :order-by.sync="orderBy"
-               @update:orderBy="x" :loading="loading" height="400"></g-table>
-    </div>
-    <!--    <div style="margin: 20px;">-->
-    <!--      <g-table :columns="columns" :data-soruce="dataSource" bordered compact :striped="false"></g-table>-->
-    <!--    </div>-->
-    <!--    <div style="margin: 20px;">-->
-    <!--      <g-pager :total-page="10" :current-page.sync="currentPage"></g-pager>-->
-    <!--    </div>-->
+  <div style="margin: 20px;">
+    <div>只能上传 300kb 以内的 png、jpeg文件</div>
+    <gulu-uploader accept="image/*" method="POST" action="http://127.0.0.1:3000/upload" name="files"
+                   :parse-response="parseResponse" :file-list.sync="fileList">
+      <g-button icon="upload">上传</g-button>
+    </gulu-uploader>
   </div>
 </template>
 
 <script>
-import GPager from './pager'
-import GTable from './table'
+import GuluUploader from "./uploader";
+import GButton from './button/button'
+import GIcon from './icon'
 
 export default {
   name: "demo",
-  components: {GPager, GTable},
+  components: {GuluUploader, GButton},
   data() {
     return {
-      currentPage: 1,
-      selected: [],
-      columns: [
-        {text: '姓名', field: 'name', width: 100},
-        {text: '分数', field: 'score', width: 100}
-      ],
-      orderBy: { // true - 开启排序, 但是不确定asc desc
-        score: 'desc'
-      },
-      loading: false,
-      dataSource: [
-        {id: 1, name: '方方', score: 100},
-        {id: 2, name: '圆圆', score: 99},
-        {id: 3, name: '张三', score: 100},
-        {id: 4, name: '李四', score: 99},
-        {id: 5, name: '超人', score: 100},
-        {id: 6, name: '蝙蝠侠', score: 99},
-        {id: 7, name: '蜘蛛侠', score: 100},
-        {id: 8, name: '钢铁侠', score: 99},
-        {id: 9, name: '蜘蛛侠', score: 100},
-        {id: 10, name: '钢铁侠', score: 99},
-        {id: 11, name: '方方', score: 100},
-        {id: 12, name: '圆圆', score: 99},
-        {id: 13, name: '张三', score: 100},
-        {id: 14, name: '李四', score: 99},
-        {id: 15, name: '超人', score: 100},
-        {id: 16, name: '蝙蝠侠', score: 99},
-        {id: 17, name: '蜘蛛侠', score: 100},
-        {id: 18, name: '钢铁侠', score: 99},
-        {id: 19, name: '蜘蛛侠', score: 100},
-        {id: 20, name: '钢铁侠', score: 99},
-        {id: 21, name: '方方', score: 100},
-        {id: 22, name: '圆圆', score: 99},
-        {id: 23, name: '张三', score: 100},
-        {id: 24, name: '李四', score: 99},
-        {id: 25, name: '超人', score: 100},
-        {id: 26, name: '蝙蝠侠', score: 99},
-        {id: 27, name: '蜘蛛侠', score: 100},
-        {id: 28, name: '钢铁侠', score: 99},
-        {id: 29, name: '蜘蛛侠', score: 100},
-        {id: 30, name: '钢铁侠', score: 99}
-      ]
+      fileList: []
     }
   },
   methods: {
-    x() {
-      this.loading = true
-      setTimeout(() => {
-        this.dataSource = this.dataSource.sort((a, b) => a.score - b.score)
-        this.loading = false
-      }, 3000)
+    parseResponse(response) {
+      let object = JSON.parse(response)
+      let url = `http://127.0.0.1:3000/preview/${object.id}`
+      return url
     }
   }
 };
--- src/table.vue
+++ src/table.vue
@@ -1,34 +1,59 @@
 <template>
-  <div class="gulu-table-wrapper" :style="{height: height + 'px'}" ref="wrapper">
-    <div :style="{height: height + 'px', overflow: 'auto'}" ref="tableWrapper">
+  <div class="gulu-table-wrapper" ref="wrapper">
+    <div :style="{height, overflow: 'auto'}" ref="tableWrapper">
       <table class="gulu-table" :class="{bordered, compact, striped: striped}" ref="table">
         <thead>
         <tr>
-          <th :style="{width: '50px'}">
-            <input type="checkbox" @change="onChangeAllItems" ref="allChecked" :checked="areAllItemsSelected"/>
-          </th>
+          <th v-if="expendField" :style="{width: '50px'}" class="gulu-table-center"></th>
+          <th v-if="checkable" :style="{width: '50px'}" class="gulu-table-center">
+            <input type="checkbox" @change="onChangeAllItems" ref="allChecked" :checked="areAllItemsSelected"/></th>
           <th :style="{width: '50px'}" v-if="numberVisible">#</th>
-          <th :style="{width: column.width+'px'}" v-for="column in columns" :key="column.field">
+          <th :style="{width: column.width + 'px'}" v-for="column in columns" :key="column.field">
             <div class="gulu-table-header">
-              {{ column.text }}
+              {{column.text}}
               <span v-if="column.field in orderBy" class="gulu-table-sorter" @click="changeOrderBy(column.field)">
-              <g-icon name="asc" :class="{active: orderBy[column.field] === 'asc'}"/>
-              <g-icon name="desc" :class="{active: orderBy[column.field] === 'desc'}"/>
-            </span>
+                <g-icon name="asc" :class="{active: orderBy[column.field] === 'asc'}"/>
+                <g-icon name="desc" :class="{active: orderBy[column.field] === 'desc'}"/>
+              </span>
             </div>
           </th>
+          <th ref="actionsHeader" v-if="$scopedSlots.default"></th>
         </tr>
         </thead>
         <tbody>
-        <tr v-for="(item, index) in dataSoruce" :key="item.id">
-          <td :style="{width: '50px'}">
-            <input type="checkbox" @change="onChangeItem(item, index, $event)" :checked="inSelectedItems(item)"/>
-          </td>
-          <td :style="{width: '50px'}" v-if="numberVisible">{{ index + 1 }}</td>
-          <template v-for="column in columns">
-            <td :style="{width: column.width + 'px'}" :key="column.field">{{ item[column.field] }}</td>
-          </template>
-        </tr>
+        <template v-for="item,index in dataSource">
+          <tr :key="item.id">
+            <td v-if="expendField" :style="{width: '50px'}" class="gulu-table-center">
+              <g-icon class="gulu-table-expendIcon" name="right"
+                      @click="expendItem(item.id)"/>
+            </td>
+            <td v-if="checkable" :style="{width: '50px'}" class="gulu-table-center">
+              <input type="checkbox" @change="onChangeItem(item, index, $event)"
+                     :checked="inSelectedItems(item)"
+              /></td>
+            <td :style="{width: '50px'}" v-if="numberVisible">{{index+1}}</td>
+            <template v-for="column in columns">
+              <td :style="{width: column.width + 'px'}" :key="column.field">
+                <template v-if="column.render">
+                  <vnodes :vnodes="column.render({value: item[column.field]})"></vnodes>
+                </template>
+                <template v-else>
+                  {{item[column.field]}}
+                </template>
+              </td>
+            </template>
+            <td v-if="$scopedSlots.default">
+              <div ref="actions" style="display: inline-block;">
+                <slot :item="item"></slot>
+              </div>
+            </td>
+          </tr>
+          <tr v-if="inExpendedIds(item.id)" :key="`${item.id}-expend`">
+            <td :colspan="columns.length + expendedCellColSpan">
+              {{item[expendField] || '空'}}
+            </td>
+          </tr>
+        </template>
         </tbody>
       </table>
     </div>
@@ -40,17 +65,31 @@
 
 <script>
 import GIcon from './icon'
-
 export default {
-  components: {GIcon},
+  components: {
+    GIcon,
+    vnodes: {
+      functional: true,
+      render: (h, context) => context.props.vnodes
+    }
+  },
   name: "GuluTable",
+  data () {
+    return {
+      expendedIds: [],
+      columns: []
+    }
+  },
   props: {
     height: {
       type: Number
     },
+    expendField: {
+      type: String
+    },
     orderBy: {
       type: Object,
-      default: () => ({})
+      default: () => ({}),
     },
     loading: {
       type: Boolean,
@@ -68,15 +107,11 @@ export default {
       type: Boolean,
       default: false
     },
-    columns: {
-      type: Array,
-      required: true
-    },
-    dataSoruce: {
+    dataSource: {
       type: Array,
       required: true,
-      validator(array) {
-        return !(array.filter(item => item.id === undefined).length > 0);
+      validator (array) {
+        return !(array.filter(item => item.id === undefined).length > 0)
       }
     },
     numberVisible: {
@@ -86,14 +121,21 @@ export default {
     bordered: {
       type: Boolean,
       default: false
+    },
+    checkable: {
+      type: Boolean,
+      default: false
     }
   },
-  data() {
-    return {
-      table2: null
-    }
-  },
-  mounted() {
+  mounted () {
+    this.columns = this.$slots.default.map(node => {
+      let {text, field, width} = node.componentOptions.propsData
+      let render = node.data.scopedSlots && node.data.scopedSlots.default
+      return {text, field, width, render}
+    })
+    let result = this.columns[0].render({value: '方方'})
+    console.log(result)
+
     let table2 = this.$refs.table.cloneNode(false)
     this.table2 = table2
     table2.classList.add('gulu-table-copy')
@@ -103,32 +145,51 @@ export default {
     this.$refs.tableWrapper.style.height = this.height - height + 'px'
     table2.appendChild(tHead)
     this.$refs.wrapper.appendChild(table2)
-    this.updateHeaderWidth()
-    this.onWindowResize = () => this.updateHeaderWidth()
-    window.addEventListener('resize', this.onWindowResize)
+
+
+    if (this.$scopedSlots.default) {
+      let div = this.$refs.actions[0]
+      let {width} = div.getBoundingClientRect()
+      let parent = div.parentNode
+      let styles = getComputedStyle(parent)
+      let paddingLeft = styles.getPropertyValue('padding-left')
+      let paddingRight = styles.getPropertyValue('padding-right')
+      let borderLeft = styles.getPropertyValue('border-left-width')
+      let borderRight = styles.getPropertyValue('border-right-width')
+      let width2 = width + parseInt(paddingRight) + parseInt(paddingRight) + parseInt(borderLeft) + parseInt(borderRight) + 'px'
+      this.$refs.actionsHeader.style.width = width2
+      this.$refs.actions.map(div => {
+        div.parentNode.style.width = width2
+      })
+
+    }
   },
-  beforeDestroy() {
+  beforeDestroy () {
     this.table2.remove()
-    window.removeEventListener('resize', this.onWindowResize)
   },
+
   computed: {
-    areAllItemsSelected() {
-      const a = this.dataSoruce.map(item => item.id).sort()
+    areAllItemsSelected () {
+      const a = this.dataSource.map(item => item.id).sort()
       const b = this.selectedItems.map(item => item.id).sort()
-      if (a.length !== b.length) {
-        return false
-      }
+      if (a.length !== b.length) { return false }
       let equal = true
       for (let i = 0; i < a.length; i++) if (a[i] !== b[i]) {
         equal = false
         break
       }
       return equal
+    },
+    expendedCellColSpan () {
+      let result = 0
+      if (this.checkable) { result += 1 }
+      if (this.expendField) { result += 1 }
+      return result
     }
   },
   watch: {
-    selectedItems() {
-      if (this.selectedItems.length === this.dataSoruce.length) {
+    selectedItems () {
+      if (this.selectedItems.length === this.dataSource.length) {
         this.$refs.allChecked.indeterminate = false
       } else if (this.selectedItems.length === 0) {
         this.$refs.allChecked.indeterminate = false
@@ -138,23 +199,17 @@ export default {
     }
   },
   methods: {
-    updateHeaderWidth() {
-      let table2 = this.table2
-      let tableHeader = Array.from(this.$refs.table.children).filter(node => node.tagName.toLowerCase() === 'thead')[0]
-      let tableHeader2
-      Array.from(table2.children).map(node => {
-        if (node.tagName.toLowerCase() !== 'thead') {
-          node.remove()
-        } else {
-          tableHeader2 = node
-        }
-      })
-      Array.from(tableHeader.children[0].children).map((th, i) => {
-        const {width} = th.getBoundingClientRect()
-        tableHeader2.children[0].children[i].style.width = width + 'px'
-      })
+    inExpendedIds (id) {
+      return this.expendedIds.indexOf(id) >= 0
     },
-    changeOrderBy(key) {
+    expendItem (id) {
+      if (this.inExpendedIds(id)) {
+        this.expendedIds.splice(this.expendedIds.indexOf(id), 1)
+      } else {
+        this.expendedIds.push(id)
+      }
+    },
+    changeOrderBy (key) {
       const copy = JSON.parse(JSON.stringify(this.orderBy))
       let oldValue = copy[key]
       if (oldValue === 'asc') {
@@ -166,109 +221,93 @@ export default {
       }
       this.$emit('update:orderBy', copy)
     },
-    inSelectedItems(item) {
+    inSelectedItems (item) {
       return this.selectedItems.filter(i => i.id === item.id).length > 0
     },
-    onChangeItem(item, index, e) {
+    onChangeItem (item, index, e) {
       let selected = e.target.checked
       let copy = JSON.parse(JSON.stringify(this.selectedItems))
       if (selected) {
         copy.push(item)
       } else {
         copy = copy.filter(i => i.id !== item.id)
-        copy.splice(copy.indexOf(item), 1)
       }
       this.$emit('update:selectedItems', copy)
     },
-    onChangeAllItems(e) {
+    onChangeAllItems (e) {
       let selected = e.target.checked
-      this.$emit('update:selectedItems', selected ? this.dataSoruce : [])
+      this.$emit('update:selectedItems', selected ? this.dataSource : [])
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-@import "styles/var";
-
+@import "var";
 $grey: darken($grey, 10%);
 .gulu-table {
   width: 100%;
   border-collapse: collapse;
   border-spacing: 0;
   border-bottom: 1px solid $grey;
-
   &.bordered {
     border: 1px solid $grey;
-
     td, th {
       border: 1px solid $grey;
     }
   }
-
   &.compact {
     td, th {
       padding: 4px;
     }
   }
-
   td, th {
     border-bottom: 1px solid $grey;
     text-align: left;
     padding: 8px;
   }
-
   &.striped {
     tbody {
       > tr {
         &:nth-child(odd) {
           background: white;
         }
-
         &:nth-child(even) {
           background: lighten($grey, 10%);
         }
       }
     }
   }
-
   &-sorter {
     display: inline-flex;
     flex-direction: column;
-    margin: 0 2px;
-
+    margin: 0 4px;
+    cursor: pointer;
     svg {
       width: 10px;
       height: 10px;
       fill: $grey;
-      cursor: pointer;
-
       &.active {
         fill: red;
       }
-
       &:first-child {
         position: relative;
         bottom: -1px;
       }
-
       &:nth-child(2) {
         position: relative;
         top: -1px;
       }
     }
   }
-
   &-header {
     display: flex;
     align-items: center;
   }
-
   &-wrapper {
     position: relative;
     overflow: auto;
   }
-
   &-loading {
     background: rgba(255, 255, 255, 0.8);
     position: absolute;
@@ -279,14 +318,12 @@ $grey: darken($grey, 10%);
     display: flex;
     justify-content: center;
     align-items: center;
-
     svg {
       width: 50px;
       height: 50px;
       @include spin;
     }
   }
-
   &-copy {
     position: absolute;
     top: 0;
@@ -294,5 +331,13 @@ $grey: darken($grey, 10%);
     width: 100%;
     background: white;
   }
+  &-expendIcon {
+    width: 10px;
+    height: 10px;
+  }
+  & &-center {
+    text-align: center;
+  }
+
 }
 </style>
