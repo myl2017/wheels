53f3d070bd5293d533471367014fc9fe452e5e56
706695013@qq.com <706695013@qq.com>
2021-10-21 10:33:20 +0800

    单元测试 修复

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,10 @@
-// const path = require('path')
-// module.exports = {
-//     css: {
-//         loaderOptions: {
-//             sass: {
-//                 prependData: '@import "./styles/_var.scss";'
-//             }
-//         }
-//     }
-// }
 const path = require('path')
 module.exports = {
-    css: {
-        loaderOptions: {
-            sass: {
-                includePaths: [path.join(__dirname, 'styles')]
-            },
-        }
-    },
+  css: {
+    loaderOptions: {
+      sass: {
+        includePaths: [path.join(__dirname, 'styles')]
+      },
+    }
+  },
 }
